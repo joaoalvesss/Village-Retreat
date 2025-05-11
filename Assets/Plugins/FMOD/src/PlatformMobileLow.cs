2daba889c0fe567fc6c71f80b0558e0deaf58c67
joaoalvesss <up202108670@up.pt>
2025-05-11 20:30:47 +0100

    working fix try

--- Assets/Plugins/FMOD/src/PlatformMobileLow.cs
+++ Assets/Plugins/FMOD/src/PlatformMobileLow.cs
@@ -44,15 +44,15 @@ namespace FMODUnity
 
         internal override bool SupportsAdditionalCPP(BuildTarget target)
         {
+        #if UNITY_IOS
             if (target == BuildTarget.iOS)
             {
                 return PlatformIOS.StaticSupportsAdditionalCpp();
             }
-            else
-            {
-                return base.SupportsAdditionalCPP(target);
-            }
+        #endif
+            return base.SupportsAdditionalCPP(target);
         }
+
 #endif
 
         internal override float Priority { get { return DefaultPriority + 1; } }

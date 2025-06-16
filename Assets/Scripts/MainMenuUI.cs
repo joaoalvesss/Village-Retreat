52dfdbf3ae0e6cf3783fe2366cd817cf3490ef94
up202103342 <emts2002@gmail.com>
2025-06-16 22:34:18 +0100

    added menu sounds

--- Assets/Scripts/MainMenuUI.cs
+++ Assets/Scripts/MainMenuUI.cs
@@ -1,18 +1,23 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using FMODUnity;
 
 public class MainMenuUI : MonoBehaviour
 {
     public GameObject optionsPanel;
     public GameObject mainMenuPanel; 
+    public string skipSound = "event:/UI/Skip";
+    public string contSound = "event:/UI/UI_hover";
 
     public void StartGame()
     {
+        RuntimeManager.PlayOneShot(skipSound, transform.position);
         SceneManager.LoadScene("Island");
     }
 
     public void OpenOptions()
     {
+        RuntimeManager.PlayOneShot(contSound, transform.position);
         optionsPanel.SetActive(true);
         if (mainMenuPanel != null)
             mainMenuPanel.SetActive(false); 
@@ -20,6 +25,7 @@ public class MainMenuUI : MonoBehaviour
 
     public void CloseOptions()
     {
+        RuntimeManager.PlayOneShot(contSound, transform.position);
         optionsPanel.SetActive(false);
         if (mainMenuPanel != null)
             mainMenuPanel.SetActive(true);
@@ -27,6 +33,7 @@ public class MainMenuUI : MonoBehaviour
 
     public void QuitGame()
     {
+        RuntimeManager.PlayOneShot(skipSound, transform.position);
         Application.Quit();
 
         #if UNITY_EDITOR

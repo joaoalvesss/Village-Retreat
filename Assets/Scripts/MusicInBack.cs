52916d45dc5b947b8f863d8de966577312e07133
IgorAndrade10 <igor-m-andrade@hotmail.com>
2025-06-17 02:00:05 +0100

    Added sfx

--- Assets/Scripts/MusicInBack.cs
+++ Assets/Scripts/MusicInBack.cs
@@ -6,11 +6,14 @@ public class MusicInBack : MonoBehaviour
     [Header("FMOD Music Settings")]
     [EventRef]
     public string musicEvent = "event:/Music/SceneMusic"; // Escreves isto no Inspector
+    public string[] backgroundMusic = { "event:/Music/music 01", "event:/Music/music_animalcrossing1", "event:/Music/music_animalcrossing2", "event:/Music/music_animalcrossing3", "event:/Music/music_animalcrossing4" };
+    private int index = 0;
 
     [Range(0f, 1f)]
     public float volume = 1.0f; // Escreves isto no Inspector também
 
     private FMOD.Studio.EventInstance musicInstance;
+    private FMOD.Studio.EventInstance backgroundInstance;
 
     void Start()
     {
@@ -18,6 +21,11 @@ public class MusicInBack : MonoBehaviour
         musicInstance = RuntimeManager.CreateInstance(musicEvent);
         musicInstance.setVolume(volume);
         musicInstance.start();
+
+        backgroundInstance = RuntimeManager.CreateInstance(backgroundMusic[index]);
+        backgroundInstance.setVolume(volume/20);
+        backgroundInstance.start();
+        backgroundInstance.release();
     }
 
     void Update()
@@ -29,11 +37,31 @@ public class MusicInBack : MonoBehaviour
         {
             musicInstance.start();
         }
+
+        FMOD.Studio.PLAYBACK_STATE playbackStatebackground;
+        backgroundInstance.getPlaybackState(out playbackStatebackground);
+
+        if (playbackStatebackground == FMOD.Studio.PLAYBACK_STATE.STOPPED)
+        {
+            if (index == 4)
+            {
+                index = 0;
+            } else
+            {
+                index++;
+            }
+            backgroundInstance = RuntimeManager.CreateInstance(backgroundMusic[index]);
+            backgroundInstance.setVolume(volume / 20);
+            backgroundInstance.start();
+            backgroundInstance.release();
+        }
     }
 
     void OnDestroy()
     {
         musicInstance.stop(FMOD.Studio.STOP_MODE.ALLOWFADEOUT);
         musicInstance.release();
+        backgroundInstance.stop(FMOD.Studio.STOP_MODE.ALLOWFADEOUT);
+        backgroundInstance.release();
     }
 }
